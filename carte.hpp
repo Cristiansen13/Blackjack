bb38e74182f5c4b9a54e2e751f11b33e6ef12b5d
Mihai-Cristian Strejaru <cristianstrejaru@gmail.com>
2023-05-27 19:43:02 +0300

    Tema 1 + 2

--- carte.hpp
+++ carte.hpp
@@ -51,3 +51,25 @@ public:
     bool IsFlipped() const;
 };
 
+
+int Card::GetValue() const {
+    if (static_cast<int>(rank) > 11)
+        return 10;
+    return static_cast<int>(rank);  
+}
+
+char Card::GetColour() const {
+    if (suit == Suit::CLUBS) {
+        return 'C';
+    } else if (suit == Suit::DIAMONDS) {
+        return 'D';
+    } else if (suit == Suit::HEARTS) {
+        return 'H';
+    } else {
+        return 'S';
+    }
+}
+
+bool Card::IsFlipped() const {
+    return isFlipped;
+}
